52a31c360a89eb9bc069c064744e76578dc4bb22
pavidhiman <pavidhiman22@gmail.com>
2026-05-04 17:03:45 -0400

    matmul update

--- cuda/matmul_tiled.cu
+++ cuda/matmul_tiled.cu
@@ -1,7 +1,7 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-#define TILE 16
+#define TILE  // tile size = 16x16 since each block of threads will load and process 16x16 chunks at a time
 
 __global__ void matmul_tiled_kernel(float *A, float *B, float *C, int M, int K, int N) {
     __shared__ float tile_A[TILE][TILE];
@@ -9,11 +9,9 @@ __global__ void matmul_tiled_kernel(float *A, float *B, float *C, int M, int K, 
 
     int row = blockIdx.y * TILE + threadIdx.y;
     int col = blockIdx.x * TILE + threadIdx.x;
-
     float sum = 0.0f;
-
     int num_tiles = (K + TILE - 1) / TILE;
-
+    // cooperative loading where each thread loads 1 element of tile_A and 1 from tile_B
     for (int t = 0; t < num_tiles; t++) {
         int a_col = t * TILE + threadIdx.x;
         if (row < M && a_col < K)
@@ -26,23 +24,19 @@ __global__ void matmul_tiled_kernel(float *A, float *B, float *C, int M, int K, 
             tile_B[threadIdx.y][threadIdx.x] = B[b_row * N + col];
         else
             tile_B[threadIdx.y][threadIdx.x] = 0.0f;
-
-        __syncthreads();
+        __syncthreads(); // makes all 256 threads in block wait 
 
         for (int k = 0; k < TILE; k++) {
             sum += tile_A[threadIdx.y][k] * tile_B[k][threadIdx.x];
         }
-
         __syncthreads();
     }
-
     if (row < M && col < N) {
         C[row * N + col] = sum;
     }
 }
 
 extern "C" {
-
 void cuda_matmul_tiled(float *host_A, float *host_B, float *host_C, int M, int K, int N) {
     float *d_A, *d_B, *d_C;
 
@@ -68,5 +62,4 @@ void cuda_matmul_tiled(float *host_A, float *host_B, float *host_C, int M, int K
     cudaFree(d_B);
     cudaFree(d_C);
 }
-
 }
